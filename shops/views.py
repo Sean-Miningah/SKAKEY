7c6351701eff966d85420adcaaaa73e3239b9608
Sean Miningah <miningahsean@gmail.com>
2021-12-29 15:32:50 +0300

    product creation responce

--- shops/views.py
+++ shops/views.py
@@ -96,5 +96,8 @@ class ProductViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
+        res = {
+            "message": "Product Succesfully created"
+        }
 
-        return Response(status=status.HTTP_201_CREATED, headers=headers)
+        return Response(res, status=status.HTTP_201_CREATED, headers=headers)
