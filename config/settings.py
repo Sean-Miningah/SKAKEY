8ef5977d6db929761ef255c72048379fa63d5ea6
Sean-Miningah <miningahsean@gmail.com>
2022-02-11 20:06:53 +0300

    csrf_trusted_origins

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS=[os.environ['WEBSITE_HOSTNAME'], '127.0.0.1']
+CSRF_TRUSTED_ORIGINS=['https://skakey-main-api.azurewebsites.net',os.environ['WEBSITE_HOSTNAME'], '127.0.0.1']
 
 ROOT_URLCONF = 'config.urls'
 
