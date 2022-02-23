1a6ebb01dba8081c42c5fb0afe59d41fa5896c96
Sean-Miningah <miningahsean@gmail.com>
2022-02-23 22:55:48 +0300

    ssl-mode required

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,7 @@ else:
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
             'HOST': os.environ.get('POSTGRES_HOST'),
             'PORT': os.environ.get('POSTGRES_PORT'),
+            'OPTIONS':{'sslmode': 'require'}
         }
     }
 
