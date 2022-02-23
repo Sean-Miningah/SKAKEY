52b91fb5b731e2f2f4c2b381972ccfa0b5f2c25c
Sean-Miningah <miningahsean@gmail.com>
2022-02-23 22:35:12 +0300

    db-username

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
+            'USER': os.environ.get('POSTGRES_USERNAME'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
             'HOST': os.environ.get('POSTGRES_HOST'),
             'PORT': os.environ.get('POSTGRES_PORT'),
